c3f056defad64c499c412b83babfe592c1c02cbe
Ramneet04 <ramneetsinghrtd@gmail.com>
2024-06-25 10:07:25 +0530

    about page staff

--- src/app/(sub_pages)/about/page.js
+++ src/app/(sub_pages)/about/page.js
@@ -4,6 +4,7 @@ import ProjectsList from "@/components/projects";
 import { projectsData } from "../../data";
 import HatModel from "@/components/models/HatModel";
 import RenderModel from "@/components/RenderModel";
+import AboutDetails from "@/components/about";
 export default function Home() {
   return (
     <>
@@ -14,6 +15,17 @@ export default function Home() {
           <HatModel/>
         </RenderModel>
       </div>
+      <div className="relative w-full h-screen flex flex-col items-center justify-center">
+        <div className="absolute flex flex-col items-center text-center top-1/2 sm:top-[60%] left-1/2 -translate-y-1/2 -translate-x-1/2">
+          <h1 className="font-bold text-6xl xs:text-6xl sm:text-6xl  lg:text-7xl text-accent">
+            Ramneet Singh
+          </h1>
+          <p className="mt-6 text-foreground text-2xl font-semibold text-yellow-400">
+            Meet the wizard behind this portfolio
+          </p>
+        </div>
+      </div>
+      <AboutDetails />
     </>
   );
 }
